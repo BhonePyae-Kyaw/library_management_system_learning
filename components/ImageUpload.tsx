e157f4996148adf9befbacb2e537dc8ba4e459fd
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2025-01-18 23:06:46 +0700

    Feat: image upload

---
+++ components/ImageUpload.tsx
@@ -0,0 +1,106 @@
+"use client";
+import React, { useRef, useState } from "react";
+import {
+  IKImage,
+  IKVideo,
+  ImageKitProvider,
+  IKUpload,
+  ImageKitContext,
+} from "imagekitio-next";
+import config from "../lib/config";
+import { Button } from "./ui/button";
+import { toast } from "@/hooks/use-toast";
+
+const authenticator = async () => {
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Authentication failed. Status: ${response.status} - ${errorText}`
+      );
+    }
+    const data = await response.json();
+
+    const { signature, expire, token } = data;
+    return data;
+  } catch (error: any) {
+    throw new Error(`Authentication failed: ${error.message}`);
+  }
+};
+
+interface Props {
+  type: "image" | "video";
+  accept: string;
+  placeholder: string;
+  folder: string;
+  variant: "dark" | "light";
+  onFileChange: (filePath: string) => void;
+  value?: string;
+}
+
+const ImageUpload = ({
+  type,
+  accept,
+  placeholder,
+  folder,
+  variant,
+  onFileChange,
+  value,
+}: Props) => {
+  const ikUploadRef = useRef(null);
+  const [file, setFile] = useState<{ filePath: string } | null>(null);
+
+  const onError = (error: any) => {
+    console.error("Error uploading file", error);
+    toast({
+      title: `${type} upload failed.`,
+      description: `There was an error uploading the file. Your ${type} can not be uploaded.`,
+      variant: "destructive",
+    });
+  };
+  const onSuccess = (res: any) => {
+    setFile(res);
+    onFileChange(res.filePath);
+
+    toast({
+      title: `${type} uploaded successfully`,
+      description: `${res.filePath} uploaded successfully!`,
+    });
+  };
+
+  return (
+    <ImageKitProvider
+      publicKey={config.env.imagekit.publicKey}
+      urlEndpoint={config.env.imagekit.urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+        fileName="test-upload.png"
+      />
+
+      <Button className="upload-btn">Upload a file</Button>
+      {file &&
+        (type === "image" ? (
+          <IKImage
+            alt={file.filePath}
+            path={file.filePath}
+            width={500}
+            height={300}
+          />
+        ) : type === "video" ? (
+          <IKVideo
+            path={file.filePath}
+            controls={true}
+            className="h-96 w-full rounded-xl"
+          />
+        ) : null)}
+    </ImageKitProvider>
+  );
+};
+
+export default ImageUpload;
