f1bec4bcfac70daaf0f5668ca246a74b1d3aee01
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2025-01-20 23:11:57 +0700

    Fix: rate limit page

--- ratelimit.ts
+++ ratelimit.ts
@@ -3,7 +3,7 @@ import redis from "./database/redis";
 
 const ratelimit = new Ratelimit({
   redis: redis,
-  limiter: Ratelimit.fixedWindow(10, "1m"),
+  limiter: Ratelimit.fixedWindow(5, "1m"),
   prefix: "@upstash/ratelimit",
 });
 
